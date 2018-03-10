ae146357199eb91551e54e574ee7482bfc7f9d89
unknown <sirishapamarthi1119@gmail.com>
2018-03-10 14:40:16 +0530

    helloworld

---
+++ main.js
@@ -0,0 +1,22 @@
+function newpage(){
+  window.open("resume.html","_self",true);
+}
+
+
+function loadJSON(file,callback){
+  var rawfile= new XMLHttpRequest();
+  rawfile.overrideMimeType("application/json");
+  rawfile.open("GET",file,true)
+rawfile.onreadystatechange=function() {
+if (rawfile.readystate==4 && rawfile.status=="200")  {
+callback(rawfile.responseText);
+}
+  }
+rawfile.send();
+}
+
+loadSON("resources/resume.json",function(){
+var data==JSON.parse(text);
+console.log(data);
+
+});
